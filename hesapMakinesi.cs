58799891b95a304a08e51f4a2fa481aa13713b2e
nesli9 <doanneslihan@gmail.com>
2022-12-16 22:30:52 +0300

    hesapMakinesi

---
+++ hesapMakinesi.cs
@@ -0,0 +1,42 @@
+using System;
+
+public class evrenselkod
+{
+    public static void Main(string[] args)
+    {
+        int sonuc = 0;
+ 
+        Console.WriteLine("Toplama İşlemi İçin +");
+        Console.WriteLine("Çıkarma İşlemi İçin -");
+        Console.WriteLine("Çarpma İşlemi İçin *");
+        Console.WriteLine("Bölme İşlemi İçin /");
+        Console.Write("Yapmak İstediğiniz işlemi Seçin : ");
+        char secim = Convert.ToChar(Console.ReadLine());
+ 
+        Console.Write("1.değeri girin : ");
+        int sayi1 = Convert.ToInt16(Console.ReadLine());
+        Console.Write("2.değeri girin : ");
+        int sayi2 = Convert.ToInt16(Console.ReadLine());
+        
+        switch (secim)
+        {
+            case '+':
+                sonuc = sayi1 + sayi2;
+                Console.WriteLine("Toplama işlemini sonucu : {0}", sonuc);
+                break;
+            case '-':
+                sonuc = sayi1 - sayi2;
+                Console.WriteLine("Çıkarma işleminin sonucu : {0}", sonuc);
+                break;
+            case '*':
+                
+                break;
+            case '/':
+                
+                break;
+            default:
+                Console.WriteLine("Hatalı Seçim Yaptınız");
+                break;
+        }
+    }
+}
